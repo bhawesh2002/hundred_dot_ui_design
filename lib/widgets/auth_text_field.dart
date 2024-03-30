48b989378af9c68dcd8d80186e1675641c099dc8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-30 19:43:33 +0530

    create AuthTextfield widget

---
+++ lib/widgets/auth_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hundred_dot_ui_design/theme/colors.dart';
+import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
+
+class AuthTextfield extends StatelessWidget {
+  final String hintText;
+  final TextInputType? inputType;
+  const AuthTextfield({super.key, required this.hintText, this.inputType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: UiSizes.widthPercent(93),
+        minHeight: UiSizes.heightPercent(7),
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(
+          UiSizes.widthPercent(10),
+        ),
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: UiSizes.widthPercent(6),
+      ),
+      child: Center(
+        child: TextField(
+          textAlign: TextAlign.center,
+          keyboardType: inputType ?? TextInputType.text,
+          decoration: InputDecoration(
+            hintText: hintText,
+          ),
+        ),
+      ),
+    );
+  }
+}
