3180dcab570a9d92838bffaeaa94edfe68c125c6
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-30 18:56:30 +0530

    Update additional1 color to additionalwhite in AppColors class

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color primary1 = Color(0xFF491AF5);
   static const Color primary2 = Color(0xFF9747FF);
   static const Color secondary = Color(0xFFBABFFC);
-  static const Color additional1 = Color(0xFFEDE0FB);
+  static const Color additionalwhite = Color(0xFFEDE0FB);
   static const Color additionalGreen = Color(0xFF7CCB5D);
   static const Color additionalRed = Color(0xFFFF6868);
   static const Color additionalYellow = Color(0xFFF9DD4B);
@@ -12,5 +12,5 @@ class AppColors {
   static const LinearGradient linearGradient = LinearGradient(
       begin: Alignment.topLeft,
       end: Alignment.bottomRight,
-      colors: [additional1, secondary]);
+      colors: [additionalwhite, secondary]);
 }
