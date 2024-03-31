62aa8668e26052e0b4f03c26190d0a423847f921
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-31 12:07:59 +0530

    Add CategoreyListTile widget

---
+++ lib/widgets/categorey_list_tile.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hundred_dot_ui_design/theme/colors.dart';
+import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
+
+class CategoreyListTile extends StatelessWidget {
+  final String label;
+  final String svgIconPath;
+  const CategoreyListTile(
+      {super.key, required this.label, required this.svgIconPath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      borderRadius: BorderRadius.circular(
+        UiSizes.widthPercent(8),
+      ),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(
+          UiSizes.widthPercent(8),
+        ),
+        child: Container(
+          width: UiSizes.widthPercent(90),
+          height: UiSizes.heightPercent(7),
+          padding: EdgeInsets.symmetric(
+            horizontal: UiSizes.widthPercent(10),
+          ),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                svgIconPath,
+                width: 25,
+                height: 25,
+                // ignore: deprecated_member_use
+                color: AppColors.primary2,
+              ),
+              SizedBox.square(
+                dimension: UiSizes.widthPercent(6),
+              ),
+              Text(
+                label,
+                style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      color: AppColors.blackColor,
+                      fontSize: UiSizes.widthPercent(3),
+                    ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
