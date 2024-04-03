827a4a7b5a23ce2a14e28db1ad2bca252761db78
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-03 10:49:53 +0530

    Update phone charge UI

--- lib/pages/home/home_body/phone_charge.dart
+++ lib/pages/home/home_body/phone_charge.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hundred_dot_ui_design/theme/colors.dart';
 import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
 import 'package:hundred_dot_ui_design/widgets/screen_app_bar.dart';
@@ -27,7 +28,7 @@ class PhoneChargePage extends StatelessWidget {
               hintText: "Add Phone Number",
             ),
             SizedBox(
-              height: UiSizes.heightPercent(4),
+              height: UiSizes.heightPercent(3),
             ),
             TextButton(
               style: TextButton.styleFrom(
@@ -46,7 +47,7 @@ class PhoneChargePage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: UiSizes.heightPercent(6),
+              height: UiSizes.heightPercent(5),
             ),
             Text(
               "Service Type",
@@ -56,7 +57,7 @@ class PhoneChargePage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: UiSizes.heightPercent(4),
+              height: UiSizes.heightPercent(3),
             ),
             Text(
               "Regular",
@@ -76,78 +77,29 @@ class PhoneChargePage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: UiSizes.heightPercent(8),
+              height: UiSizes.heightPercent(6),
             ),
-            const PhoneChargeTextField(
-              hintText: "Type Charge Amount : \$",
-            ),
-            SizedBox(
-              height: UiSizes.heightPercent(3),
-            ),
-            SizedBox(
-              width: UiSizes.widthPercent(70),
-              child: GridView.count(
-                crossAxisCount: 3,
-                childAspectRatio: 1.5,
-                shrinkWrap: true,
-                children: [
-                  Center(
-                    child: Text(
-                      "\$3.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      "\$5.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      "\$8.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      "\$10.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      "\$15.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      "\$20.00",
-                      style: TextStyle(
-                        fontSize: UiSizes.widthPercent(4),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ],
+            const ChargeAmountWidget(),
+            const Expanded(child: SizedBox()),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: AppColors.primary1,
+                minimumSize: Size(
+                  UiSizes.widthPercent(50),
+                  UiSizes.heightPercent(6),
+                ),
               ),
-            )
+              onPressed: () {},
+              child: Text(
+                "Confirm",
+                style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+            SizedBox(
+              height: UiSizes.heightPercent(2),
+            ),
           ],
         ),
       ),
@@ -185,6 +137,62 @@ class PhoneChargeTextField extends StatelessWidget {
               fontSize: UiSizes.widthPercent(4),
               fontWeight: FontWeight.normal,
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChargeAmountWidget extends StatelessWidget {
+  const ChargeAmountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> amount = ["3.00", "5.00", "8.00", "10.00", "15.00", "20.00"];
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(UiSizes.widthPercent(3)),
+      child: SizedBox(
+        width: UiSizes.widthPercent(92),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: UiSizes.widthPercent(10),
+              vertical: UiSizes.heightPercent(1)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Type Charge Amount: \$",
+                    hintStyle: TextStyle(
+                      fontSize: UiSizes.widthPercent(3.5),
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: UiSizes.widthPercent(70),
+                height: 1,
+                color: Colors.grey.shade300,
+              ),
+              GridView.count(
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                childAspectRatio: 1.5,
+                children: List.generate(
+                  6,
+                  (index) => Center(
+                    child: Text("\$${amount[index]}"),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
