531c0a20ca636df0854d861e36f8c33f12305e5d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-05 13:42:34 +0530

    Add custom text styles and colors to the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hundred_dot_ui_design/pages/onboarding/onboarding.dart';
+import 'package:hundred_dot_ui_design/theme/colors.dart';
 import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
 
 void main() {
@@ -14,6 +15,40 @@ class MainApp extends StatelessWidget {
     UiSizes.init(context); //initialize UiSizes
     return MaterialApp(
       theme: ThemeData(
+        primaryColor: AppColors.primary1,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: InputBorder.none,
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            color: AppColors.whiteColor,
+            fontWeight: FontWeight.bold,
+            fontSize: UiSizes.sizeFromWidth(10, 100),
+          ),
+          titleMedium: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: UiSizes.widthPercent(5),
+          ),
+          titleSmall: TextStyle(
+            color: Colors.black,
+            fontSize: UiSizes.widthPercent(3),
+            fontWeight: FontWeight.w600,
+          ),
+          displaySmall: TextStyle(
+            fontSize: UiSizes.sizeFromWidth(6, 100),
+            color: AppColors.whiteColor,
+            fontWeight: FontWeight.bold,
+          ),
+          labelSmall: TextStyle(
+            color: AppColors.whiteColor,
+            fontSize: UiSizes.widthPercent(4),
+            fontWeight: FontWeight.w600,
+          ),
+          headlineSmall: TextStyle(
+            fontSize: UiSizes.widthPercent(5),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         fontFamily: 'HelveticaNeue',
       ),
       home: const OnBoardingScreen(),
