3474b9acac809c5e899e0bca26d5f60f2e333e1d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-07 14:50:02 +0530

    refactoring changes

--- lib/pages/home/home_body/phone_charge.dart
+++ lib/pages/home/home_body/phone_charge.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hundred_dot_ui_design/pages/home/home_body/phone_charge_payment.dart';
 import 'package:hundred_dot_ui_design/theme/colors.dart';
 import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
+import 'package:hundred_dot_ui_design/widgets/charge_amount_widget_card.dart';
 import 'package:hundred_dot_ui_design/widgets/screen_app_bar.dart';
 
 class PhoneChargePage extends StatelessWidget {
@@ -79,7 +80,9 @@ class PhoneChargePage extends StatelessWidget {
             SizedBox(
               height: UiSizes.heightPercent(6),
             ),
-            const ChargeAmountWidget(),
+            const ChargeAmountWidgetCard(
+              hintText: "Type Charge Amount",
+            ),
             const Expanded(child: SizedBox()),
             TextButton(
               style: TextButton.styleFrom(
@@ -140,62 +143,6 @@ class PhoneChargeTextField extends StatelessWidget {
               fontSize: UiSizes.widthPercent(4),
               fontWeight: FontWeight.normal,
             ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ChargeAmountWidget extends StatelessWidget {
-  const ChargeAmountWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> amount = ["3.00", "5.00", "8.00", "10.00", "15.00", "20.00"];
-    return Material(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(UiSizes.widthPercent(3)),
-      child: SizedBox(
-        width: UiSizes.widthPercent(92),
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-              horizontal: UiSizes.widthPercent(10),
-              vertical: UiSizes.heightPercent(1)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: "Type Charge Amount: \$",
-                    hintStyle: TextStyle(
-                      fontSize: UiSizes.widthPercent(3.5),
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                width: UiSizes.widthPercent(70),
-                height: 1,
-                color: Colors.grey.shade300,
-              ),
-              GridView.count(
-                crossAxisCount: 3,
-                shrinkWrap: true,
-                childAspectRatio: 1.5,
-                children: List.generate(
-                  6,
-                  (index) => Center(
-                    child: Text("\$${amount[index]}"),
-                  ),
-                ),
-              )
-            ],
           ),
         ),
       ),
