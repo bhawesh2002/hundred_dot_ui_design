c79979636dcb930c732db02c70a22bd3999dcebd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-07 14:49:46 +0530

    Move ChargeAmountWidget to its own file

---
+++ lib/widgets/charge_amount_widget_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hundred_dot_ui_design/utilis/ui_sizes.dart';
+
+class ChargeAmountWidgetCard extends StatelessWidget {
+  final String hintText;
+  const ChargeAmountWidgetCard({super.key, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> amount = ["3.00", "5.00", "8.00", "10.00", "15.00", "20.00"];
+    return Material(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(UiSizes.widthPercent(3)),
+      child: SizedBox(
+        width: UiSizes.widthPercent(92),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: UiSizes.widthPercent(10),
+              vertical: UiSizes.heightPercent(1)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "$hintText: \$",
+                    hintStyle: TextStyle(
+                      fontSize: UiSizes.widthPercent(3.5),
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: UiSizes.widthPercent(70),
+                height: 1,
+                color: Colors.grey.shade300,
+              ),
+              GridView.count(
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                childAspectRatio: 1.5,
+                children: List.generate(
+                  6,
+                  (index) => Center(
+                    child: Text("\$${amount[index]}"),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
